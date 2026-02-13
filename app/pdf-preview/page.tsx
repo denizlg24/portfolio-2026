ef84ea0edd7714ca632037ca15ae3286346a9be8
Deniz Gunes <denizlg24@gmail.com>
2026-02-13 11:40:41 +0000

    fix(pdf): added padding on the top of pdf header

--- app/pdf-preview/page.tsx
+++ app/pdf-preview/page.tsx
@@ -16,9 +16,9 @@ export default async function AdminNotesPdfPreviewPage({
     <div className="w-full flex flex-col items-center -mt-26">
       {showHeader && (
         <>
-          <div className="w-full max-w-7xl flex flex-row justify-between items-center gap-2 font-calistoga">
+          <div className="w-full max-w-7xl flex flex-row justify-between items-center gap-2 font-calistoga pt-2">
             <p className="text-xs pl-3">denizlg24</p>
-            <p className="text-xs">{title}</p>
+            <p className="text-xs font-mono">{title}.md</p>
             <p className="text-xs pr-3">{new Date().toLocaleDateString()}</p>
           </div>
           <Separator className="my-1 w-full" />
